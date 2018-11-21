984559d2d0e190ae32f33e2b2a392e75cca4dce7
Prasek <212434152@logon.ds.ge.com>
2018-11-21 15:48:35 +0100

    new clas

---
+++ src/main/java/com/kar/kar/KarApplication.java
@@ -0,0 +1,12 @@
+package com.kar.kar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarApplication.class, args);
+	}
+}
